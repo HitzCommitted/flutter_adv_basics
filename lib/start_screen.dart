207f929dd8a3a31bb8228c7e1aef69b6405f196b
HitzCommitted <hitzcommitted@gmail.com>
2023-11-15 09:45:25 +0300

    basic working screen

---
+++ lib/start_screen.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class StartScreen extends StatelessWidget {
+  const StartScreen({super.key});
+  @override
+  Widget build(context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 300,
+            color: const Color.fromARGB(150, 255, 255, 255),
+          ),
+          const SizedBox(
+            height: 80,
+          ),
+          const Text(
+            'Welcome to Quizzing With Hitz',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 26,
+            ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          OutlinedButton.icon(
+            onPressed: () {},
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.white,
+            ),
+            icon: const Icon(
+              Icons.arrow_right_alt,
+            ),
+            label: const Text(
+              'Start Quiz',
+              style: TextStyle(
+                fontSize: 22,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
